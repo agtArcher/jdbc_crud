89301555ae42748898c04d77b2d2b4d85d81be63
agtArcher <stranger9905@gmail.com>
2020-08-26 21:52:23 +0300

    configured model classes

--- src/main/java/model/Auto.java
+++ src/main/java/model/Auto.java
@@ -1,4 +1,26 @@
 package model;
 
 public class Auto {
+    private int autoId;
+    private String model;
+    private String prodYear;
+    private int userId;
+
+    public Auto(String model, String prodYear, int userId) {
+        this.model = model;
+        this.prodYear = prodYear;
+        this.userId = userId;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getProdYear() {
+        return prodYear;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,52 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private List<Auto> autos;
+
+    public User(String firstName, String lastName, int age) {
+        id = -1;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        autos = new ArrayList<>();
+    }
+
+    public User(int id, String firstName, String lastName, int age, List<Auto> autos) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.autos = autos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<Auto> getAutos() {
+        return autos;
+    }
+
+    public void setAutos(List<Auto> autos) {
+        this.autos = autos;
+    }
 }
